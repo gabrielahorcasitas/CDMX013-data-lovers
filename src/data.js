f61ef2368809d65a6a634d4d663b6cd7972efff1
gabri <gchorcasitas@gmail.com>
2022-08-19 12:36:52 -0500

    filtros combinados primera parte

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // Filtrado de tipo
     export const filterByType =(condition,pokemonList)=>{
-      let filterType = pokemonList.filter(pokemon => pokemon.type[0] == condition.toLowerCase() || pokemon.type[1] == condition.toLowerCase());
+      let filterType = pokemonList.filter(pokemon => pokemon.type.includes(condition.toLowerCase()));//[0] == condition.toLowerCase() || pokemon.type[1] == condition.toLowerCase());
       return filterType;
     }
 
@@ -85,7 +85,7 @@
      }
 
 //Funciones para estadística
-// "special-attack": ["name", "base-damage", "energy", "move-duration-seg"];
+// dtaos por usar: "special-attack": ["name", "base-damage", "energy", "move-duration-seg"];
 
 //Función para calcular STAB (agregar 20% al daño si son del mismo tipo)
  export const calculatorSTAB = (attack,typeattack,typePokemon) => {
@@ -117,38 +117,3 @@ export const calculatorEPS = (energy, time) =>{
     return eps;
 }
 
- //Estas son otras opciones
-
-   /*export const filterByType =(condition,pokemonList) => {
-          let filtered = [];
-          for (let i = 0; i < pokemonList.length; i++){
-            let arrayTypes = pokemonList[i].type;
-            for (var j = 0; j < arrayTypes.length; j++) {
-              if(arrayTypes[j] == condition.toLowerCase()){
-                  filtered.push(pokemonList[i]);
-                }
-              }
-            }
-            return filtered;
-          }*/
-
-  /* let regionFilter= pokemonList;
-  let filterRegion = [];
-  for (let i = 0; i < regionFilter.length; i++) {
-    if (regionFilter[i].generation.name == condition) {
-        filterRegion.push(regionFilter[i]);
-    }
-  }
-  console.log( filterRegion);*/
-
-/*export const filterByRegion = (filterValue, pokemonList, condition) =>{
-  let filteredPokemon = [];
-  if(condition == "region"){
-    filteredPokemon = pokemonList.filter((current) => {
-      return current.generation.name == filterValue.toLowerCase();
-    });
-  }
-  return filteredPokemon;
-}*/
-
-//export default data
